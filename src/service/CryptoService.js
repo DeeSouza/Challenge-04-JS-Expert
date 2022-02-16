69a16843b4a47f5fb204823a9434d8aaba59b4da
Diego Souza <dee-souza@hotmail.com>
2022-02-16 09:24:14 -0300

    added variable limit in cryptoservice for request api

--- src/service/CryptoService.js
+++ src/service/CryptoService.js
@@ -3,17 +3,19 @@ import CryptoRepository from "../repository/CryptoRepository.js";
 
 class CryptoService {
   #page;
+  #limit;
   #hasData;
 
   constructor({ repository } = {}) {
     this.repository = repository || new CryptoRepository();
     this.#page = 1;
+    this.#limit = 5;
     this.#hasData = true;
   }
 
   async *list() {
     while (this.#hasData) {
-      const { data } = await this.repository.list(this.#page);
+      const { data } = await this.repository.list(this.#page, this.#limit);
 
       if (!data) this.#hasData = false;
 
